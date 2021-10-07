85151681e5537befdc93bbb6776405217c1912d1
Mavis Francia <18331943+mavisfrancia@users.noreply.github.com>
2021-10-07 00:15:07 -0500

    test: 💍 add basic tests for components

---
+++ client/src/components/tooltip/Tooltip.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import Tooltip from "./Tooltip";
+
+test("it renders children", () => {
+  const containedText = "My Test Div";
+  render(
+    <Tooltip text="Tooltip text">
+      <div>{containedText}</div>
+    </Tooltip>
+  );
+  expect(screen.getByText(containedText)).toBeInTheDocument();
+});
+
+test("it displays tooltip text", () => {
+  const tooltipText = "Tooltip text";
+  const containedText = "My Test Div";
+  render(
+    <Tooltip text={tooltipText}>
+      <div>{containedText}</div>
+    </Tooltip>
+  );
+  expect(screen.getByText(tooltipText)).toBeInTheDocument();
+});
